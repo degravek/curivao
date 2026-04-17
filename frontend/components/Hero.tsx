aae6f93592930ce6cb3dc2f35abc911ff26f3185
degravek <degravek@gmail.com>
2026-04-17 15:28:27 -0400

    add change

--- frontend/components/Hero.tsx
+++ frontend/components/Hero.tsx
@@ -3,7 +3,6 @@ import Image from "next/image";
 export default function Hero() {
   return (
     <section className="relative w-full bg-white flex items-center justify-center py-6 overflow-hidden">
-      <p>Impact-Site-Verification: 081190c2-4b2f-4cdf-ad77-bdefaec5ad03</p>
       {/* Full image, no cropping */}
       <Image
         src="/hero.png"
